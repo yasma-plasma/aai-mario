4db666fc412442f07380044f1f11bc1a6d544333
yasma-plasma <90143093+yasma-plasma@users.noreply.github.com>
2021-11-26 16:13:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
